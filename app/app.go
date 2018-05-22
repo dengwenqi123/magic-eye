e8e42897e8765ec66bd343d0377d8c361eac09e3
Admin <wenqi@megacredit.cc>
2018-05-22 16:08:39 +0800

    first commit project

---
+++ app/app.go
@@ -0,0 +1,123 @@
+package app
+
+import (
+	bam "github.com/cosmos/cosmos-sdk/baseapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/wire"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+	abci "github.com/tendermint/abci/types"
+	cmn "github.com/tendermint/tmlibs/common"
+	dbm "github.com/tendermint/tmlibs/db"
+	"github.com/tendermint/tmlibs/log"
+
+	"encoding/json"
+	"github.com/dengwenqi123/magic-eye/mgycoin/types"
+	"github.com/dengwenqi123/magic-eye/x/auth"
+)
+
+const (
+	appName = "MagicEyeApp"
+)
+
+type MagicEyeApp struct {
+	*bam.BaseApp
+	cdc *wire.Codec
+
+	//Keys to accesss the substores
+	keyMain    *sdk.KVStoreKey
+	KeyAccount *sdk.KVStoreKey
+
+	//Manage getting and setting accounts
+	accountMapper sdk.AccountMapper
+	coinKeeper    bank.Keeper
+}
+
+func NewMagicEyeApp(logger log.Logger, db dbm.DB) *MagicEyeApp {
+
+	//Create app-level codec for txs and accounts.
+	var cdc = MakeCodec()
+
+	var app = &MagicEyeApp{
+		BaseApp:    bam.NewBaseApp(appName, cdc, logger, db),
+		cdc:        cdc,
+		keyMain:    sdk.NewKVStoreKey("main"),
+		KeyAccount: sdk.NewKVStoreKey("acc"),
+	}
+
+	//Define the accountMapper.
+	app.accountMapper = auth.NewAccountMapper(
+		cdc,
+		app.KeyAccount,      // target store
+		&types.MgyAccount{}, // prototype
+	)
+
+	//
+	app.coinKeeper = bank.NewKeeper(app.accountMapper)
+	app.Router().AddRoute("bank", bank.NewHandler(app.coinKeeper))
+
+	//
+	app.SetInitChainer(app.initChainer)
+	app.MountStoresIAVL(app.keyMain, app.KeyAccount)
+	app.SetAnteHandler(auth.NewAnteHandler(app.accountMapper, auth.BurnFeeHandler))
+	err := app.LoadLatestVersion(app.keyMain)
+	if err != nil {
+		cmn.Exit(err.Error())
+	}
+	return app
+}
+
+//Custom tx codec
+func MakeCodec() *wire.Codec {
+	var cdc = wire.NewCodec()
+	wire.RegisterCrypto(cdc) //Register crypto.
+	sdk.RegisterWire(cdc)    // Register Msgs
+	bank.RegisterWire(cdc)
+
+	//register custom MgyAccount
+	cdc.RegisterInterface((*sdk.Account)(nil), nil)
+	cdc.RegisterConcrete(&types.MgyAccount{}, "basecoin/Account", nil)
+	return cdc
+
+}
+
+func (app *MagicEyeApp) initChainer(ctx sdk.Context, req abci.RequestInitChain) abci.ResponseInitChain {
+	stateJSON := req.AppStateBytes
+
+	genesisState := new(types.GenesisState)
+	err := app.cdc.UnmarshalJSON(stateJSON, genesisState)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, gacc := range genesisState.Accounts {
+		acc, err := gacc.ToMGyAccount()
+		if err != nil {
+			panic(err)
+		}
+		app.accountMapper.SetAccount(ctx, acc)
+	}
+	return abci.ResponseInitChain{}
+}
+
+//Custom logic for state export
+func (app *MagicEyeApp) ExportAppStateJSON() (appState json.RawMessage, err error) {
+	//ctx := app.NewContext(true,abci.Header{})
+	//app.NewContext(true,abci.Header{})
+	ctx := app.NewContext(true, abci.Header{})
+
+	accounts := []*types.GenesisAccount{}
+	appendAccount := func(acc sdk.Account) (stop bool) {
+		account := &types.GenesisAccount{
+			Address: acc.GetAddress(),
+			Coins:   acc.GetCoins(),
+		}
+		accounts = append(accounts, account)
+		return false
+	}
+	app.accountMapper.IterateAccounts(ctx, appendAccount)
+
+	genState := types.GenesisState{
+		Accounts: accounts,
+	}
+	return wire.MarshalJSONIndent(app.cdc, genState)
+}
---
+++ types/account.go
@@ -0,0 +1,80 @@
+package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/wire"
+	"go/types"
+)
+var _ sdk.Account = (*MgyAccount)(nil)
+
+
+type MgyAccount struct {
+	auth.BaseAccount
+	Name string `json:"name"`
+}
+
+func (acc MgyAccount) GetName() string {return  acc.Name}
+func (acc *MgyAccount) SetName(name string) {acc.Name = name}
+
+//Get the AccountDecoder function for the custom MgyAcount
+//获取自定义MgyAccount的AccountDecoder函数
+
+func GetAccountDecoder(cdc *wire.Codec) sdk.AccountDecoder {
+	return func(accBytes []byte) (res sdk.Account, err error) {
+		if len(accBytes) == 0 {
+			return nil, sdk.ErrTxDecode("accBytes are empty")
+		}
+		acct := new(sdk.Account)
+		err = cdc.UnmarshalBinaryBare(accBytes, &acct)
+		if err != nil {
+			panic(err)
+		}
+		return acct, err
+	}
+
+}
+
+//_________________________________________________
+
+//State to Unmarshal
+type GenesisState struct {
+	Accounts []*GenesisAccount `json:"accounts"`
+}
+
+//GenesisAccount doesn't need pubkey or sequence
+type GenesisAccount struct {
+	Name string `json:"name"`
+	Address sdk.Address `json:"address"`
+	Coins sdk.Coins `json:"coins"`
+}
+
+func NewGenesisAccount(aa *MgyAccount) *GenesisAccount {
+	return &GenesisAccount{
+		Name:aa.Name,
+		Address:aa.Address,
+		Coins:aa.Coins.Sort(),
+	}
+}
+
+// convert GenesisAccount to MgyAccount
+func (ga *GenesisAccount) ToMGyAccount() (acc *MgyAccount,err error) {
+	baseAcc := auth.BaseAccount{
+		Address:ga.Address,
+		Coins:ga.Coins.Sort(),
+	}
+	return &MgyAccount{
+		BaseAccount:baseAcc,
+		Name:ga.Name,
+	},nil
+}
+
+
+
+
+
+
+
+
+
+
